23f92f768459cfac2e977a7a33db6af30394740c
Salaou-Deen Paraiso <pariso03henri@gmail.com>
2020-10-20 21:38:37 +0100

    - Add exp models

--- henri/home/models.py
+++ henri/home/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Color(models.Model):
+    id = models.AutoField(primary_key=True)
+    class_name = models.CharField(max_length=50)
+    des = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.des
+
+
+class Experience(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    link = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateField()
+    color = models.ForeignKey(Color, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
