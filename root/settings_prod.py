f2b1d97811c9929785c430b81e1181c36b66e18f
Salaou-Deen Paraiso <pariso03henri@gmail.com>
2020-10-22 21:34:50 +0100

    - add prod config

---
+++ root/settings_prod.py
@@ -0,0 +1,20 @@
+from root.settings import *
+import dj_database_url
+
+ALLOWED_HOSTS = ['sadih.herokuapp.com']
+DEBUG = False
+TEMPLATES_DEBUG = False
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DATABASES['default'] = dj_database_url.config()
+
+CLOUDINARY_STORAGE = {
+
+    'CLOUD_NAME': os.environ.get('CLOUDNAME'),
+    'API_KEY': os.environ.get('APIKEY'),
+    'API_SECRET': os.environ.get('APISECRET')
+
+}
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+MEDIA_URL = '/media-henry/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media-henry/')
