b7d3a087c81dbdb18495a06b355a1366f6b967da
Salaou-Deen Paraiso <pariso03henri@gmail.com>
2020-10-22 21:33:10 +0100

    - add latest posts

--- henri/home/views.py
+++ henri/home/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from henri.home.models import Experience
+from henri.blog.models import Post
 
 
 def home(request):
     exps = Experience.objects.all()
-    context = {'exps': exps}
+    lasts = Post.objects.order_by('created_at')[:3]
+    context = {'exps': exps, 'lasts': lasts}
     return render(request, 'home.html', context)
