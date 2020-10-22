439c46c1d7129a882dbcefe1fd3f092f5ebd89fa
Salaou-Deen Paraiso <pariso03henri@gmail.com>
2020-10-22 21:37:09 +0100

    - add category, comment models

--- henri/blog/models.py
+++ henri/blog/models.py
@@ -2,8 +2,37 @@ from django.db import models
 from tinymce import HTMLField
 
 
-class Post(models.Model):
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255,unique=True)
     banner = models.ImageField(upload_to='blog/post_banner')
+    post = HTMLField('Post')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    view = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    body = models.TextField()
+    author_name = models.CharField(max_length=50)
+    author_mail = models.CharField(max_length=50)
+    author_pic = models.ImageField(default='comments/d.png')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.author_name
