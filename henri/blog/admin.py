704e7ac4c6b358131f289c140e6a493e91f89489
Salaou-Deen Paraiso <pariso03henri@gmail.com>
2020-10-22 21:39:50 +0100

    - models registration

--- henri/blog/admin.py
+++ henri/blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from henri.blog.models import Post, Category, Comment
 
-# Register your models here.
+admin.site.register(Post)
+admin.site.register(Category)
+admin.site.register(Comment)
