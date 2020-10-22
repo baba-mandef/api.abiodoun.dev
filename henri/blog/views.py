f52765fe54d6e6c5d3fdf7bdb047b68e0176bbe2
Salaou-Deen Paraiso <pariso03henri@gmail.com>
2020-10-22 21:33:33 +0100

    - add posts list views

--- henri/blog/views.py
+++ henri/blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from henri.blog.models import Post, Category
 
-# Create your views here.
+
+def posts(request):
+    all_posts = Post.objects.all
+    cats = Category.objects.all()
+
+    context = {'posts': all_posts, 'cats': cats}
+    return render(request, 'post.html', context)
