d67450cf915bdc44b70fd386d6417d884b491e4d
Salaou-Deen Paraiso <pariso03henri@gmail.com>
2020-10-24 07:57:44 +0100

    - add post details view

--- henri/blog/views.py
+++ henri/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from henri.blog.models import Post, Category
+from henri.blog.models import Post, Category, Comment
 
 
 def posts(request):
@@ -8,3 +8,13 @@ def posts(request):
 
     context = {'posts': all_posts, 'cats': cats}
     return render(request, 'post.html', context)
+
+
+def details(request, slug):
+    post = Post.objects.get(slug=slug)
+    cats = Category.objects.all()
+    post.view += 1
+    post.save()
+    comments = Comment.objects.filter(id=post.id)
+    context = {'post': post, 'cats': cats, 'comments': comments}
+    return render(request, 'post-details.html', context)
