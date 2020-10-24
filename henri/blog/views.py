c45381288d0a11b8ae7dce8b7d0375b6e1f44231
Salaou-Deen Paraiso <pariso03henri@gmail.com>
2020-10-24 12:51:42 +0100

    - add comment form view

--- henri/blog/views.py
+++ henri/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from henri.blog.models import Post, Category, Comment
+from henri.blog.models import (Post, Category, Comment)
+from henri.blog.forms import CommentForm
 
 
 def posts(request):
@@ -15,8 +16,15 @@ def details(request, slug):
     cats = Category.objects.all()
     post.view += 1
     post.save()
+    form = CommentForm
+    if request.method == 'POST':
+        form = CommentForm(data=request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
     comments = Comment.objects.filter(id=post.id)
-    context = {'post': post, 'cats': cats, 'comments': comments}
+    context = {'post': post, 'cats': cats, 'comments': comments, 'form': form}
     return render(request, 'post-details.html', context)
 
 
@@ -24,5 +32,6 @@ def categories(request, category):
     posts = Post.objects.filter(category__title=category)
     cats = Category.objects.all()
     cat_title = category
+
     context = {'posts': posts, 'cats': cats, 'cat_title': cat_title}
     return render(request, 'category.html', context)
