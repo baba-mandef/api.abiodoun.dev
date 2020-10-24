b8885a47db9bf8892ad15fac0b1c130516e4192c
Salaou-Deen Paraiso <pariso03henri@gmail.com>
2020-10-24 11:35:37 +0100

    - add category view

--- henri/blog/views.py
+++ henri/blog/views.py
@@ -18,3 +18,11 @@ def details(request, slug):
     comments = Comment.objects.filter(id=post.id)
     context = {'post': post, 'cats': cats, 'comments': comments}
     return render(request, 'post-details.html', context)
+
+
+def categories(request, category):
+    posts = Post.objects.filter(category__title=category)
+    cats = Category.objects.all()
+    cat_title = category
+    context = {'posts': posts, 'cats': cats, 'cat_title': cat_title}
+    return render(request, 'category.html', context)
