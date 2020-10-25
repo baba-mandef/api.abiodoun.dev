c3b5d7902394ff5d19b2460f786454c4dc05bc04
Salaou-Deen Paraiso <pariso03henri@gmail.com>
2020-10-25 03:27:23 +0100

    - Refactoring

--- henri/blog/urls.py
+++ henri/blog/urls.py
@@ -4,5 +4,5 @@ from henri.blog.views import (posts, details, categories)
 urlpatterns = [
     path('', posts, name='posts'),
     path('post/<slug:slug>', details, name='details'),
-    path('category/<str:category>', categories, name='category' )
+    path('category/<str:category>', categories, name='category'),
 ]
