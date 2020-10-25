a58e88dab12e8ffa5ccb2a428514d9157c1e7596
Salaou-Deen Paraiso <pariso03henri@gmail.com>
2020-10-25 03:32:53 +0100

    - Viewcounter model registration

--- henri/blog/admin.py
+++ henri/blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from henri.blog.models import Post, Category, Comment
+from henri.blog.models import (Post, Category, Comment, ViewCount)
 
 admin.site.register(Post)
 admin.site.register(Category)
 admin.site.register(Comment)
+admin.site.register(ViewCount)
