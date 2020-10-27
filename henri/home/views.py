080f54e9edbc500b5a2dd922b72c7ec965779a6c
Salaou-Deen Paraiso <pariso03henri@gmail.com>
2020-10-27 09:10:12 +0100

    - add filter

--- henri/home/views.py
+++ henri/home/views.py
@@ -5,6 +5,7 @@ from henri.blog.models import Post
 
 def home(request):
     exps = Experience.objects.all()
-    lasts = Post.objects.order_by('created_at')[:3]
+    latest = Post.objects.filter(published=True)
+    lasts = latest.order_by('created_at')[:3]
     context = {'exps': exps, 'lasts': lasts}
     return render(request, 'home.html', context)
