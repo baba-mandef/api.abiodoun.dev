5a4d1d2ba5aef00a31f10d2769ab72cbabf8f14b
henri-dev <pariso03henri@gmail.com>
2021-07-16 17:14:57 +0100

    - add check_comment.py

---
+++ henri/utils/check_comment.py
@@ -0,0 +1,10 @@
+import re
+
+def checking(comment):
+    check_link =  re.findall(r'http+', comment)
+    print(check_link)
+    if len(check_link)>0:
+        return True
+    else:
+        return False
+
