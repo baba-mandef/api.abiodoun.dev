73a4753ffd7a00a45c3bb50d3bfc83af160635a6
henri-dev <pariso03henri@gmail.com>
2021-10-29 10:52:27 +0100

    - update serializers.py

--- henri/blog/serializers.py
+++ henri/blog/serializers.py
@@ -5,22 +5,23 @@ from henri.blog.models import (Post, Comment, Category, ViewCount)
 class PostSerializer(ModelSerializer):
     class Meta:
         model = Post
-        field = '__all__'
+        fields = ['id', 'title', 'slug', 'banner', 'post', 'created_at', 'updated_at', 'category', 'published', 'view']
 
 
 class CommentSerializer(ModelSerializer):
     class Meta:
         model = Comment
-        field = '__all__'
+        fields = ['id', 'created_at', 'body', 'author_name', 'author_mail', 'author_pic', 'post']
 
 
 class CategorySerializer(ModelSerializer):
-    class  Meta:
+    class Meta:
         model = Category
-        field = '__all__'
+        fields = ['id', 'title']
 
 
 class ViewSerializer(ModelSerializer):
     class Meta:
         model = ViewCount
-        field='__all__'
+        fields = ['id', 'ip_adress', 'post']
+
