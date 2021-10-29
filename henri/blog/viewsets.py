188991de3f17eb67826834292ee994cf02a2c09f
henri-dev <pariso03henri@gmail.com>
2021-10-29 10:52:45 +0100

    - update viewsets.py

--- henri/blog/viewsets.py
+++ henri/blog/viewsets.py
@@ -1,6 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from henri.blog.serializers import PostSerializer, CommentSerializer, ViewSerializer, CategorySerializer
-from henri.blog.models import Comment
+from henri.blog.models import Comment, Category, ViewCount, Post
 
 
 class CommentViewSet(ModelViewSet):
@@ -8,12 +8,12 @@ class CommentViewSet(ModelViewSet):
     http_method_names = ['get', 'post']
 
     def get_queryset(self):
-        post_pk = self.kwargs['post_id']
-
+        post_pk = self.request.query_params['post']
         return Comment.objects.filter(post=post_pk)
 
 
 class CategoryViewSet(ModelViewSet):
+    queryset = Category.objects.all
     serializer_class = CategorySerializer
     http_method_names = ['get']
 
@@ -22,7 +22,12 @@ class ViewViewSet(ModelViewSet):
     serializer_class = ViewSerializer
     http_method_names = ['get', 'post']
 
+    def get_queryset(self):
+        post_pk = self.request.query_params['post']
+        return ViewCount.objects.filter(post=post_pk)
+
 
 class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
     serializer_class = PostSerializer
     http_method_names = ['get', 'post', 'put']
