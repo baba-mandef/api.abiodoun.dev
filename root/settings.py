ed696c167b2d74d798bf619b4e268df724c4192d
henri-dev <pariso03henri@gmail.com>
2021-12-02 23:11:29 +0100

    - add cors config

--- root/settings.py
+++ root/settings.py
@@ -50,12 +50,14 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'crispy_forms',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -112,6 +114,12 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+CORS_ALLOWED_ORIGINS  = [
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:8000",
+]
+
 
 
 # Internationalization
