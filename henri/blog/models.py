21ecf02c0e7db39e035557f8c281e5b8d2236aac
henri-dev <pariso03henri@gmail.com>
2021-12-02 23:15:44 +0100

    - update category models

--- henri/blog/models.py
+++ henri/blog/models.py
@@ -3,8 +3,8 @@ from tinymce import HTMLField
 
 
 class Category(models.Model):
-    id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=255)
+    # id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100,primary_key=True)
 
     def __str__(self):
         return self.title
