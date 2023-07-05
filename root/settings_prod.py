2afad9b689d0636d1ec95932b5769a7eac5a36d5
ptahemdjehuty <pariso03henri@gmail.com>
2023-07-05 15:21:28 +0100

    - cloudinary config

--- root/settings_prod.py
+++ root/settings_prod.py
@@ -1,20 +1,23 @@
-from root.settings import *
-import dj_database_url
+from .settings import *
 
-ALLOWED_HOSTS = ['sadih.herokuapp.com']
+#ALLOWED_HOSTS = ['sadih.herokuapp.com']
 DEBUG = True
 TEMPLATES_DEBUG = True
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DATABASES['default'] = dj_database_url.config()
+#DATABASES['default'] = dj_database_url.config()
 
-CLOUDINARY_STORAGE = {
+CLOUDINARY = {
 
-    'CLOUD_NAME': os.environ.get('CLOUDNAME'),
-    'API_KEY': os.environ.get('APIKEY'),
-    'API_SECRET': os.environ.get('APISECRET')
+    'cloud_name': os.environ.get('CLOUDNAME'),
+    'api_key': os.environ.get('APIKEY'),
+    'api_secret': os.environ.get('APISECRET'),
+    'api_proxy': 'http://proxy.server:3128'
 
 }
+CLOUDINARY_URL=os.environ.get('CLOUDINARY_URL')
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 MEDIA_URL = '/media-henry/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media-henry/')
+
+
--- root/urls.py
+++ root/urls.py
@@ -17,12 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tinymce/', include('tinymce.urls')),
-    # path('', include('henri.home.urls')),
-    path('api/v1/', include('henri.routers'))
+    
+    path('api/v1/', include('baba.routers')),
      path('openapi/', get_schema_view( title="SchoolMan", description="API developers"
     ), name='openapi-schema'),
     path('docs', TemplateView.as_view(template_name='swagger-ui.html', extra_context={'schema_url':'openapi-schema'}), name='swagger-ui'),
