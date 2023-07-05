f64384d8ad03d46cc998250e921ac46cd6dc76fb
ptahemdjehuty <pariso03henri@gmail.com>
2023-07-05 15:19:00 +0100

    - refactoring baba_blog

---
+++ baba/blog/admin.py
@@ -0,0 +1,45 @@
+from django.contrib import admin
+from baba.blog.models import (Post, Category, Comment, ViewCount)
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
+
+
+class PostResource(resources.ModelResource):
+
+    class Meta:
+        model = Post
+class PostAdmin(ImportExportModelAdmin):
+    resource_class = PostResource
+
+class CResource(resources.ModelResource):
+
+    class Meta:
+        model = Category
+
+class CAdmin(ImportExportModelAdmin):
+    resource_class = CResource
+
+class CommentResource(resources.ModelResource):
+
+    class Meta:
+        model = Comment
+
+class CommentAdmin(ImportExportModelAdmin):
+    resource_class = CommentResource
+
+class ViewResource(resources.ModelResource):
+
+    class Meta:
+        model = ViewCount
+
+
+class ViewAdmin(ImportExportModelAdmin):
+    resource_class = ViewResource
+
+
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(ViewCount, ViewAdmin)
---
+++ baba/blog/viewsets.py
@@ -0,0 +1,97 @@
+from rest_framework.views import APIView
+from baba.utils.get_ip import visitor_ip_address
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from telegram.ext import (Updater, CallbackContext)
+from root.settings import token, chat
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+# from django.shortcuts import get_object_or_404
+from baba.blog.serializers import PostSerializer, CommentSerializer, CategorySerializer
+from baba.blog.models import Comment, Category, ViewCount, Post
+
+update = Updater(token, use_context=True)
+job = update.job_queue
+
+
+class CommentViewSet(ModelViewSet):
+    serializer_class = CommentSerializer
+    http_method_names = ['get', 'post']
+
+    def get_queryset(self):
+        queryset = Comment.objects.all()
+        post_pk = self.request.query_params.get('post')
+        if post_pk is not None:
+            queryset = Comment.objects.filter(post=post_pk)
+        return queryset
+
+
+class CategoryViewSet(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    http_method_names = ['get']
+
+
+class PostViewSet(ModelViewSet):
+    serializer_class = PostSerializer
+    http_method_names = ['get', 'post', 'put']
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    authentication_classes = [TokenAuthentication]
+
+    def get_queryset(self):
+        queryset = Post.objects.all().order_by('-created_at')
+        slug = self.request.query_params.get('slug')
+        category = self.request.query_params.get('category')
+        if slug is not None:
+            queryset = Post.objects.filter(slug=slug)
+        elif category is not None:
+            queryset = Post.objects.filter(category__title=category)
+        return queryset
+
+
+class GetUserIP(APIView):
+
+    def get(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return Response(ip)
+
+
+class ViewCounter(APIView):
+
+    def get(self, request):
+        ip = visitor_ip_address(request)
+        post_pk = request.query_params.get('post')
+        post = Post.objects.get(pk=post_pk)
+        view = ViewCount.objects.filter(post=post_pk, ip_adress=ip)
+
+        def callback_view(context: CallbackContext):
+            context.bot.send_message(chat_id=chat,
+                                     text='Reader ip : {}\n Post title : {} \n First time : True \n link : '
+                                          'https://baba-dev.tech/blog/post/{} \n views : {}'.format(ip, post.title, post.slug, post.view))
+
+        def callback_review(context: CallbackContext):
+            context.bot.send_message(chat_id=chat,
+                                     text='Reader ip : {}\n Post title : {} \n First time : False \n link : '
+                                          'https://baba-dev.tech/blog/post/{} \n views : {}'.format(ip, post.title, post.slug, post.view))
+        if view and post.published:
+            post.view = post.view
+            post.save()
+            job.run_once(callback_review, 1)
+            return Response("Already read")
+        elif not view and post.published:
+            post.view = post.view + 1
+            new_ip = ViewCount(post=post, ip_adress=ip)
+            new_ip.save()
+            job.run_once(callback_view, 1)
+            post.save()
+            return Response("New reader")
+
+
+
+
+
