0b550b606dd447f726c619876caad77ca120624f
ptahemdjehuty <pariso03henri@gmail.com>
2023-07-05 15:16:58 +0100

    - add router

---
+++ baba/routers.py
@@ -0,0 +1,20 @@
+from rest_framework.routers import DefaultRouter
+from baba.blog.viewsets import (PostViewSet, CategoryViewSet, CommentViewSet, GetUserIP, ViewCounter)
+from baba.work.viewsets import WorkViewset
+from django.urls import path, include
+
+router = DefaultRouter(trailing_slash=False)
+
+router.register(r'blog/post', PostViewSet, basename='post')
+router.register(r'blog/category', CategoryViewSet, basename='category')
+router.register(r'blog/comment', CommentViewSet, basename='comment')
+# router.register(r'blog/view', ViewViewSet, basename='view')
+router.register(r'work', WorkViewset, basename='work')
+
+
+urlpatterns = [
+    path('getIp/', GetUserIP.as_view()),
+    path('countView/', ViewCounter.as_view()),
+    path('', include(router.urls)),
+   
+]
