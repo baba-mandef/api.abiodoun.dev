52844ef70caedb73cf83fdcb5a1b972020455076
ptahemdjehuty <pariso03henri@gmail.com>
2023-07-30 20:49:05 +0100

    ➕ add new message post endpoint to router

--- abiodoun/routers.py
+++ abiodoun/routers.py
@@ -1,6 +1,7 @@
 from rest_framework.routers import DefaultRouter
 from abiodoun.blog.viewsets import (PostViewSet, CategoryViewSet, CommentViewSet)
 from abiodoun.work.viewsets import WorkViewset
+from abiodoun.message.viewsets import MessageViewSet
 from django.urls import path, include
 
 router = DefaultRouter(trailing_slash=False)
@@ -8,6 +9,7 @@ router = DefaultRouter(trailing_slash=False)
 router.register(r'blog/post', PostViewSet, basename='post')
 router.register(r'blog/category', CategoryViewSet, basename='category')
 router.register(r'blog/comment', CommentViewSet, basename='comment')
+router.register(r'message/new', MessageViewSet, basename='message')
 # router.register(r'blog/view', ViewViewSet, basename='view')
 router.register(r'work', WorkViewset, basename='work')
 
