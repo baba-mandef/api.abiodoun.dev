383dec989e02bae5574f65bf73e636357f38a66a
ptahemdjehuty <pariso03henri@gmail.com>
2023-07-30 21:13:08 +0100

    ✨ create message model class

--- abiodoun/message/models.py
+++ abiodoun/message/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from abiodoun.abstract.models import AbiodounObject
 
-# Create your models here.
+
+class Message(AbiodounObject):
+    sender_name = models.CharField(max_length=150)
+    sender_email = models.EmailField()
+    body = models.TextField()
