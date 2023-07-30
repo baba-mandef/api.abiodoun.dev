566e612cb440e74dab82e71fcba00044f3a1bf70
ptahemdjehuty <pariso03henri@gmail.com>
2023-07-30 22:28:26 +0100

    ✏️ update message content

--- abiodoun/message/viewsets.py
+++ abiodoun/message/viewsets.py
@@ -3,7 +3,7 @@ from abiodoun.message.serializers import MessageSerializer
 from rest_framework.status import HTTP_201_CREATED
 from rest_framework.response import Response
 from telegram.ext import ApplicationBuilder, ContextTypes
-from django.conf import settings
+from django.template.loader import render_to_string
 from abiodoun.utils.mail_client import Email_Message
 
 
@@ -16,14 +16,33 @@ class MessageViewSet(ModelViewSet):
 
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
-        sender = serializer.validated_data.get('sender')
+        sender = serializer.validated_data.get('sender_name')
         sender_email = serializer.validated_data.get('sender_email')
         body = serializer.validated_data.get('body')
+        message =  f"""
+        Cher {str(sender)},
+        Votre message a bien été reçu et je vous en suis reconnaissant.
+        Je souhaite vous informer que votre message a été enregistré 
+        dans ma boîte de réception et que je prends très au sérieux 
+        toutes les demandes que je reçois. Je vais étudier attentivement
+        votre message et je vous répondrai dans les plus brefs délais.
 
+        Si votre demande est urgente ou si vous avez besoin d'une réponse
+        rapide, je vous conseille de me contacter directement par
+        téléphone au +229 66173930.
+
+        Encore une fois, merci de l'intérêt que vous portez à mon travail.
+        Je suis ravi de pouvoir échanger avec vous et j'espère que nous
+        pourrons collaborer ensemble.
+
+        Cordialement,
+        Abiodoun Paraïso
+        Développeur web
+        """
         message = Email_Message(
             [sender_email],
-            f"Merci de nous avoir contacter cher(e) {sender} !",
-            body
+            f"Merci de nous avoir contacter cher(e) {str(sender)} !",
+            message
         )
         message.send()
 
