7da283fde18ed6cc119ec2522a6b62be720fd311
ptahemdjehuty <pariso03henri@gmail.com>
2023-08-06 21:32:40 +0100

    ➕ add str method to message model

--- abiodoun/message/models.py
+++ abiodoun/message/models.py
@@ -6,3 +6,6 @@ class Message(AbiodounObject):
     sender_name = models.CharField(max_length=150)
     sender_email = models.EmailField()
     body = models.TextField()
+
+    def __str__(self) -> str:
+        return f'{self.sender_name}->{self.sender_email}'
