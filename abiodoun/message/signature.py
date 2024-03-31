958f1cf619669ae757f32d9e602c7073b15bcc90
baba-mandef <contact@abiodoun.dev>
2024-03-31 15:04:13 +0100

    📝 add email signature

---
+++ abiodoun/message/signature.py
@@ -0,0 +1,91 @@
+signature = """ 
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Signature</title>
+<style>
+    body {
+        font-family: Arial, sans-serif;
+        line-height: 1.6;
+        color: #000a40;
+        background-color: #ffffff;
+        padding: 20px;
+    }
+    .highlight {
+        background-color: #ff7624;
+        display: inline-block;
+    }
+</style>
+</head>
+<body>
+    <div style="font-size:16px;" id="signature-wrapper">
+        <table cellspacing="0" cellpadding="0" style="font-size: medium; font-family: Arial, sans-serif;">
+            <tr>
+                <td style="vertical-align:middle" width="90">
+                    <img width="90" style="max-width:90px; margin-right:20px;" src="https://avatars.githubusercontent.com/u/50630643?v=4" alt="Avatar">
+                </td>
+                <td style="vertical-align:middle;">
+                    <h2 style="margin:0; font-size:18px; font-weight:600; color:#000a40;">Abiodoun PARAÏSO</h2>
+                    <p style="margin:0; line-height:22px; color:#000a40; font-size: 14px; font-weight: 500;">Développeur de logiciel</p>
+                    <p style="margin:0; line-height:22px; color:#000a40; font-size: 14px; font-weight: 500;">Baba Mandef .inc</p>
+                    <p style="margin:0; line-height:22px; color:#000a40; font-size: 14px;">L'innovation au service de l'humain.</p>
+                </td>
+                <td width="30"></td>
+                <td class="color-divider__Divider-sc-1h38qjv-0 llIisW" style="width:1px; border-bottom:medium; border-left:1px solid #ff7624;" height="auto" width="1"></td>
+                <td width="30"></td>
+                <td style="vertical-align:middle;">
+                    <table cellspacing="0" cellpadding="0" style="font-size:medium; font-family:Arial, sans-serif;">
+                        <tr height="25" style="vertical-align:middle">
+                            <td style="vertical-align:middle" width="30">
+                                <div style="width:30px;">
+                                    <img width="13" style="display:block; background-color:#ff7624;" alt="mobilePhone" src="https://cdn2.hubspot.net/hubfs/53/tools/email-signature-generator/icons/phone-icon-2x.png">
+                                </div>
+                            </td>
+                            <td style="padding:0px; color:#000a40;">
+                                <a style="text-decoration:none; font-size:12px; color:#000a40;" href="tel:+22998879049">229 98879049</a>
+                            </td>
+                        </tr>
+                        <tr height="25" style="vertical-align:middle">
+                            <td style="vertical-align:middle" width="30">
+                                <div style="width:30px;">
+                                    <img width="13" style="display:block; background-color:#ff7624;" alt="emailAddress" src="https://cdn2.hubspot.net/hubfs/53/tools/email-signature-generator/icons/email-icon-2x.png">
+                                </div>
+                            </td>
+                            <td style="padding:0px;">
+                                <a style="text-decoration:none; font-size:12px; color:#000a40;" href="mailto:contact@abiodoun.dev">contact@abiodoun.dev</a>
+                            </td>
+                        </tr>
+                        <tr height="25" style="vertical-align:middle">
+                            <td style="vertical-align:middle" width="30">
+                                <div style="width:30px;">
+                                    <img width="13" style="display:block; background-color:#ff7624;" alt="website" src="https://cdn2.hubspot.net/hubfs/53/tools/email-signature-generator/icons/link-icon-2x.png">
+                                </div>
+                            </td>
+                            <td style="padding:0px;">
+                                <a style="text-decoration:none; font-size:12px; color:#000a40;" href="https://abiodoun.dev" target="_blank">abiodoun.dev</a>
+                            </td>
+                        </tr>
+                        <tr height="25" style="vertical-align:middle">
+                            <td style="vertical-align:middle" width="30">
+                                <div style="width:30px;">
+                                    <img width="13" style="display:block; background-color:#ff7624;" alt="address" src="https://cdn2.hubspot.net/hubfs/53/tools/email-signature-generator/icons/address-icon-2x.png">
+                                </div>
+                            </td>
+                            <td style="padding:0px;">
+                                <span style="font-size:12px; color:#000a40;">106 H Aglogbe centre</span>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
+
+
+"""
