5bd785e80e7d367a5745f820c60c4e6cee044a9b
rezolusoft <info@rezolusoft.com>
2024-10-30 14:59:39 -0400

    update

--- root/prod.py
+++ root/prod.py
@@ -32,8 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 ALLOWED_HOSTS = ['rezolusoft.com', 'www.rezolusoft.com', 'abiodoun.rezolusoft.com', 'www.abiodoun.rezolusoft.com', 'api.abiodoun.dev']
 
 
-STATIC_ROOT = 
-os.path.join('/home/rezogsyk/api.abiodoun.dev/static')
+STATIC_ROOT = os.path.join('/home/rezogsyk/api.abiodoun.dev/static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 CLOUDINARY = {
